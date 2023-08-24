2d50f3d879c92cb851313f98c38a9100727883bf
jennifer-l-k <jennifer.krueger0400@gmail.com>
2023-08-25 00:55:25 +0200

    create excel-classifier related to https://github.com/fabian-z/word-classifier

--- excel-classifier/Properties/Resources.Designer.cs
+++ excel-classifier/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace excel_classifier.Properties {
+    using System;
     
     
     /// <summary>
@@ -18,7 +19,7 @@ namespace excel_classifier.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -56,6 +57,96 @@ namespace excel_classifier.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_32 {
+            get {
+                object obj = ResourceManager.GetObject("icon_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_64 {
+            get {
+                object obj = ResourceManager.GetObject("icon_64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_green_32 {
+            get {
+                object obj = ResourceManager.GetObject("icon_green_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_green_64 {
+            get {
+                object obj = ResourceManager.GetObject("icon_green_64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_orange_32 {
+            get {
+                object obj = ResourceManager.GetObject("icon_orange_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_orange_80 {
+            get {
+                object obj = ResourceManager.GetObject("icon_orange_80", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_red_32 {
+            get {
+                object obj = ResourceManager.GetObject("icon_red_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_red_64 {
+            get {
+                object obj = ResourceManager.GetObject("icon_red_64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logo_filled {
+            get {
+                object obj = ResourceManager.GetObject("logo_filled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
--- excel-classifier/ThisAddIn.cs
+++ excel-classifier/ThisAddIn.cs
@@ -7,27 +7,173 @@ using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
 using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using static System.Collections.Specialized.BitVector32;
+using System.Diagnostics;
+using System.ComponentModel.Design;
+using System.Data;
 
 namespace excel_classifier
 {
     public partial class ThisAddIn
     {
-        void Application_WorkbookBeforeSave(Microsoft.Office.Interop.Excel.Workbook Wb, bool SaveAsUI, ref bool Cancel)
+        internal enum Classification
         {
-            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet);
-            Excel.Range firstRow = activeWorksheet.get_Range("A1:A1");
-            firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
-            Excel.Range newFirstRow = activeWorksheet.get_Range("A1:A1");
-            newFirstRow.Value2 = "This text was added by using code";
-            newFirstRow.Font.Color = Color.Red;
+            None,
+            White,
+            Green,
+            Amber,
+            Red
         }
+
+        private static string classificationProperty = "Classification";
+
+        // User control
+        private UserControl _usr;
+        // Custom task pane
+        private Microsoft.Office.Tools.CustomTaskPane _taskPane;
+
+        private static readonly Dictionary<Classification, string> classificationHeader = new Dictionary<Classification, string> {
+            { Classification.White, "Classified: TLP White" },
+            { Classification.Green, "Classified: TLP Green" },
+            { Classification.Amber, "Classified: TLP Amber" },
+            { Classification.Red, "Classified: TLP Red" },
+        };
+
+        private static readonly Dictionary<Classification, Microsoft.Office.Interop.Excel.XlRgbColor> classificationTextColor = new Dictionary<Classification, Microsoft.Office.Interop.Excel.XlRgbColor> {
+             
+            { Classification.White, Excel.XlRgbColor.rgbDarkGray },
+            { Classification.Green, Excel.XlRgbColor.rgbGreen },
+            { Classification.Amber, Excel.XlRgbColor.rgbOrange },
+            { Classification.Red, Excel.XlRgbColor.rgbRed},
+        };
+
+        private static readonly Dictionary<Classification, string> classificationProperties = new Dictionary<Classification, string> {
+            { Classification.White, "TLP:WHITE" },
+            { Classification.Green, "TLP:GREEN" },
+            { Classification.Amber, "TLP:AMBER" },
+            { Classification.Red, "TLP:RED" },
+        };
+
+        internal void ToggleTaskPane()
+        {
+            if (_taskPane != null)
+            {
+                _taskPane.Visible = !_taskPane.Visible;
+            }
+        }
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            this.Application.WorkbookBeforeSave += new Microsoft.Office.Interop.Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+            this.Application.WorkbookBeforeSave += new Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+
+
+            //Create an instance of the user control
+            _usr = new TaskPane();
+            // Connect the user control and the custom task pane 
+            _taskPane = CustomTaskPanes.Add(_usr, "Classifier TLP");
+            _taskPane.Width = 300;
+            _taskPane.Visible = false;
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+        }
+
+        void Application_WorkbookBeforeSave(Excel.Workbook Wb, bool SaveAsUI, ref bool Cancel)
+        {
+            Classification classification = GetClassification();
+            if (classification == Classification.None)
+            {
+                Cancel = true;
+                MessageBox.Show("Please classify document before saving. See the Add-In / Classifier Toolbar Menu.", "Classifier prevented saving", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // re-apply changes as defense in depth
+                Classify(classification);
+            }
+        }
+
+        Classification GetClassification()
+        {
+            string classificationString = ReadDocumentProperty(classificationProperty);
+            if (!classificationProperties.ContainsValue(classificationString))
+            {
+                return Classification.None;
+            }
+            else
+            {
+                Classification key = classificationProperties.FirstOrDefault(x => x.Value == classificationString).Key;
+                return key;
+            }
+        }
+
+        internal void Classify(Classification classification)
+        {
+
+            Debug.Print(classification.ToString());
+
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet);
+
+            if (Application.Interactive)
+            {
+                // Exit cell-edit mode for Excel (no API), otherwise Range.Insert and headerRange.Value will fail
+                Excel.Range r = Globals.ThisAddIn.Application.ActiveCell;
+                Globals.ThisAddIn.Application.ActiveWindow.Activate();
+                SendKeys.Flush();
+                SendKeys.SendWait("{ENTER}");
+                r.Select(); // restore selection
+            }
+
+            if (GetClassification() == Classification.None) {
+                // Shift down everything by one row to make space for header
+                activeWorksheet.Rows[1].Insert(XlInsertShiftDirection.xlShiftDown, XlInsertFormatOrigin.xlFormatFromLeftOrAbove);
+            }
+
+            // Set custom document property
+            SetDocumentProperty(classificationProperty, classificationProperties[classification]);
+
+
+            // Set classification notice
+            activeWorksheet.Rows[1].Cells.Clear();
+            Range headerRange = activeWorksheet.Range["A1"];
+            headerRange.Font.Color = classificationTextColor[classification];
+            headerRange.Font.Size = 16;
+            //headerRange.Cells.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignLeft;
+
+            headerRange.Value = classificationHeader[classification];
+        }
+
+        private string ReadDocumentProperty(string propertyName)
+        {
+            Office.DocumentProperties properties;
+            properties = (Office.DocumentProperties)this.Application.ActiveWorkbook.CustomDocumentProperties;
+
+            foreach (Office.DocumentProperty prop in properties)
+            {
+                if (prop.Name == propertyName)
+                {
+                    return prop.Value.ToString();
+                }
+            }
+            return null;
+        }
+
+        private void SetDocumentProperty(string propertyName, string value)
+        {
+            Office.DocumentProperties properties;
+            properties = (Office.DocumentProperties)this.Application.ActiveWorkbook.CustomDocumentProperties;
+
+            if (ReadDocumentProperty(propertyName) != null)
+            {
+                properties[propertyName].Delete();
+            }
+
+            properties.Add(propertyName, false,
+                Microsoft.Office.Core.MsoDocProperties.msoPropertyTypeString,
+                value);
         }
 
         #region VSTO generated code
@@ -41,7 +187,9 @@ namespace excel_classifier
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
         }
-        
+
+
         #endregion
     }
 }
+ 
